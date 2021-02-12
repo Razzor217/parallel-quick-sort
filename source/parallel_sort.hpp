0a463ff058e707ed2618a36d176ea87634764965
Max Beddies <ubyjy@student.kit.edu>
2021-02-12 17:46:39 +0100

    add todos

---
+++ source/parallel_sort.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+/*
+ * - normal interface + configurable interface
+ * - configurable helper classes 
+ *     - (de-)activate sorted checking
+ *     - change base case size
+ *     - ...
+ * - separate sequential implementation
+ * - base case sorter depending on size
+ *     - small: insertion sort
+ *     - medium: sequential quick sort
+ * - tasks? barrier synch?
+ */
+
+namespace sequential
+{
+    
+}
+
+namespace parallel
+{
+
+}

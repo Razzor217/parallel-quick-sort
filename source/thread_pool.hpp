8e17c80e9b9f517610c41be6d869462142aa0eed
Max Beddies <ubyjy@student.kit.edu>
2021-02-20 00:08:39 +0100

    add thread pool implementation

--- source/thread_pool.hpp
+++ source/thread_pool.hpp
@@ -11,8 +11,11 @@
 #pragma once
 
 #include <thread>
-#include <limits>
 #include <memory>
+#include <functional>
+
+#include <limits>
+#include <vector>
 
 #include "detail/barrier.hpp"
 
@@ -23,15 +26,13 @@ namespace qsmb
     public:
         using sync = detail::barrier;
 
-        int num_threads() {return 0; }
-
         template <class F>
         void operator ()(F&& func, int num_threads = std::numeric_limits<int>::max())
         {
             num_threads = std::min(num_threads, num_threads());
             if (num_threads > 1)
             {
-
+                impl_->run(func, num_threads);
             }
             else
             {
@@ -39,12 +40,77 @@ namespace qsmb
             }
         }
 
+        inline sync& get_sync() { return impl_->get_sync(); }
+        inline int num_threads() { return impl_->num_threads(); }
+
     private:
         class impl
         {
         public:
+            void main_loop(const int thread_id)
+            {
+                for (;;)
+                {
+                    barrier_.arrive_and_wait();
+                    if (done_)
+                    {
+                        break;
+                    }
+                    if (thread_id < num_threads_)
+                    {
+                        func_(thread_id, num_threads_);
+                    }
+                    barrier_.arrive_and_wait();
+                }
+            }
+
+            template <class F>
+            void run(F&& func, const int num_threads)
+            {
+                func_ = func;
+                num_threads_ = num_threads;
+                sync_.set_num_threads(num_threads_);
+
+                /*
+                 * Start parallel execution
+                 */
+                barrier_.arrive_and_wait();
+                func_(0, num_threads_);
+                barrier_.arrive_and_wait();
+            }
+
+            inline sync& get_sync() { return sync_; }
+            inline int num_threads() { return threads_.size() + 1; }
+
+            impl(const int num_threads) :
+                sync_ {std::max(1, num_threads)},
+                barrier_ {std::max(1, num_threads)},
+                num_threads_ {num_threads}
+            {
+                threads_.reserve(num_threads_ - 1);
+                for (int i {1}; i < num_threads_; ++i)
+                {
+                    threads_.emplace_back(&impl::main_loop, this, i);
+                }
+            }
+
+            ~impl()
+            {
+                done_ = true;
+                barrier_.arrive_and_wait();
+                for (auto& thread : threads_)
+                {
+                    thread.join();
+                }
+            }
+
+        private:
             sync sync_;
-            detail::barrier pool_barrier;
+            detail::barrier barrier_;
+            std::vector<std::thread> threads_;
+            std::function<void(int, int)> func_;
+            int num_threads_;
+            bool done_;
         };
 
         std::unique_ptr<impl> impl_;

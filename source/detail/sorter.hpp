6208f4c268345f38c6f2ec0e52996de79e47d59c
Max Beddies <ubyjy@student.kit.edu>
2021-02-22 17:27:49 +0100

    doc sorter typedefs

--- source/detail/sorter.hpp
+++ source/detail/sorter.hpp
@@ -20,9 +20,28 @@ namespace qsmb
         class sorter
         {
         public:
+            /**
+             * @brief Iterator type for the input data
+             * 
+             */
             using iterator = typename config::iterator;
+
+            /**
+             * @brief Difference type of the iterator
+             * 
+             */
             using difference_type = typename config::difference_type;
+
+            /**
+             * @brief Value type of the input data
+             * 
+             */
             using value_type = typename config::value_type;
+
+            /**
+             * @brief Comparison function object
+             * 
+             */
             using less = typename config::less;
 
             /**
@@ -32,11 +51,6 @@ namespace qsmb
              * @param end Iterator to the end of the thread-local input
              */
             void sequential(iterator begin, iterator end);
-
-            /*
-            * TODO:
-            * complete constructor
-            */ 
 
             /**
              * @brief Construct a new sorter object
